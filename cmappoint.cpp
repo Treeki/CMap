8b7f5366beb648e7fabdfae34176f70796599a62
Treeki <treeki@gmail.com>
2013-03-02 04:50:09 +0100

    add CLine and CDiamond classes, and cartesian coords support to CMapPoint

--- cmappoint.cpp
+++ cmappoint.cpp
@@ -1,4 +1,5 @@
 #include "cmappoint.h"
+#include <cmath>
 
 uint qHash(const CMapPoint &point) {
 	return (point.x << 11) | point.y;
@@ -10,3 +11,85 @@ QDebug operator<<(QDebug dbg, const CMapPoint &point) {
 	dbg.nospace() << "CMapPoint(" << point.x << "," << point.y << ")";
 	return dbg.maybeSpace();
 }
+
+QDebug operator<<(QDebug dbg, const CLine &line) {
+	const char *angleString = "_";
+	switch (line.angle) {
+	case INVALID_LINE_ANGLE: angleString = "INVALID_LINE_ANGLE"; break;
+	case VERTICAL: angleString = "VERTICAL"; break;
+	case HORIZONTAL: angleString = "HORIZONTAL"; break;
+	case NW_SE: angleString = "NW_SE"; break;
+	case NE_SW: angleString = "NE_SW"; break;
+	}
+
+	dbg.nospace() <<
+					 "CLine(" << angleString << "," <<
+					 line.start << "," << line.end << ")";
+	return dbg.maybeSpace();
+}
+
+
+
+void CLine::snapToPoints(const CMapPoint &anchor, const CMapPoint &other) {
+	int xDiff = anchor.cartesianX() - other.cartesianX();
+	int yDiff = anchor.cartesianY() - other.cartesianY();
+
+	double angle = std::atan2(yDiff, xDiff) * (180 / 3.14159265);
+
+	// 0: North West
+	// 45: North
+	// 90: North East
+	// 135: East
+	// 180: South East
+	// -135: South
+	// -90: South West
+	// -45: West
+
+	// This code is kind of crap. Oh well. It works.. hopefully?
+	int rawDirection = int(floor((angle + 22.5) / 45.0));
+	OrdinalDirection direction;
+	if (rawDirection >= 0)
+		direction = (OrdinalDirection)rawDirection;
+	else
+		direction = (OrdinalDirection)(8 + rawDirection);
+
+	int length = std::floor(std::sqrt((xDiff*xDiff) + (yDiff*yDiff)));
+
+	CMapPoint endPoint = anchor;
+
+	switch (direction) {
+	// Simple isometric directions
+	case NORTH_WEST:
+		for (int i = 0; i < length; i++)
+			endPoint.moveNW();
+		break;
+	case NORTH_EAST:
+		for (int i = 0; i < length; i++)
+			endPoint.moveNE();
+		break;
+	case SOUTH_EAST:
+		for (int i = 0; i < length; i++)
+			endPoint.moveSE();
+		break;
+	case SOUTH_WEST:
+		for (int i = 0; i < length; i++)
+			endPoint.moveSW();
+		break;
+	// Cardinal directions
+	case WEST:
+		endPoint.x -= (length * 2);
+		break;
+	case EAST:
+		endPoint.x += (length * 2);
+		break;
+	case NORTH:
+		endPoint.y -= length;
+		break;
+	case SOUTH:
+		endPoint.y += length;
+		break;
+	}
+
+	tryAssign(anchor, endPoint);
+}
+
--- cmappoint.h
+++ cmappoint.h
@@ -2,15 +2,31 @@
 #define CMAPPOINT_H
 
 #include <QtGlobal>
+#include <functional>
+
+enum OrdinalDirection {
+	NORTH_WEST = 0,
+	NORTH = 1,
+	NORTH_EAST = 2,
+	EAST = 3,
+	SOUTH_EAST = 4,
+	SOUTH = 5,
+	SOUTH_WEST = 6,
+	WEST = 7
+};
 
 struct CMapPoint {
 	quint16 x, y;
 
-	CMapPoint() { }
+	CMapPoint() { x = 0xFFFF; }
 	CMapPoint(int _x, int _y) { x = _x; y = _y; }
 
 	bool operator==(CMapPoint &other) const {
 		return (x == other.x) && (y == other.y);
+	}
+
+	void clearRightWall() {
+		x &= ~1;
 	}
 
 	void moveNW() {
@@ -36,9 +52,184 @@ struct CMapPoint {
 			x += 2;
 		y++;
 	}
+
+	void makeInvalid() {
+		x = 0xFFFF;
+		y = 0xFFFF;
+	}
+
+	bool isValid() const {
+		return (x != 0xFFFF);
+	}
+
+	bool intersectsLine(int withX, int withY, int multiplier) const {
+		int xDiff = withX - x;
+		int yDiff = (withY - y) * multiplier;
+
+		if ((y & 1) == 0) {
+			if ((withY & 1) == 0)
+				return (xDiff == yDiff);
+			else
+				return ((xDiff - 1) == yDiff);
+		} else {
+			if ((withY & 1) == 0)
+				return ((xDiff + 1) == yDiff);
+			else
+				return (xDiff == yDiff);
+		}
+	}
+
+	bool intersectsLineNWSE(int withX, int withY) const {
+		return intersectsLine(withX, withY, 1);
+	}
+	bool intersectsLineNESW(int withX, int withY) const {
+		return intersectsLine(withX, withY, -1);
+	}
+
+	int cartesianX() const {
+		return ((x & ~1) + y) / 2;
+	}
+	int cartesianY() const {
+		return (y - (x & ~1) + 1) >> 1;
+	}
+
+	// NOTE: This one *might* not work properly...
+	void setCartesian(int cX, int cY) {
+		int work = (cX - cY) + 1;
+		x = work - (work % 2);
+		y = cX + cY;
+	}
+};
+
+enum LineAngle {
+	INVALID_LINE_ANGLE,
+	VERTICAL,
+	HORIZONTAL,
+	NW_SE,
+	NE_SW
+};
+
+struct CLine {
+	CMapPoint start, end;
+	LineAngle angle;
+
+	bool operator==(CLine &other) const {
+		return (angle == other.angle) &&
+				(start == other.start) && (end == other.end);
+	}
+
+	void makeInvalid() {
+		angle = INVALID_LINE_ANGLE;
+	}
+	bool isValid() const {
+		return (angle != INVALID_LINE_ANGLE);
+	}
+
+	void forEach(std::function<void(const CMapPoint &)> func) {
+		CMapPoint iter;
+
+		switch (angle) {
+		case INVALID_LINE_ANGLE:
+			break;
+
+		case VERTICAL:
+			for (iter = start; iter.y <= end.y; iter.y++)
+				func(iter);
+
+			break;
+
+		case HORIZONTAL:
+			for (iter = start; iter.x <= end.x; iter.x += 2)
+				func(iter);
+
+			break;
+
+		case NW_SE:
+			for (iter = start; iter.y <= end.y; iter.moveSE())
+				func(iter);
+
+			break;
+
+		case NE_SW:
+			for (iter = start; iter.y <= end.y; iter.moveSW())
+				func(iter);
+
+			break;
+		}
+	}
+
+	int size() const {
+		switch (angle) {
+		case INVALID_LINE_ANGLE:
+			return 0;
+		case VERTICAL:
+			return (end.y - start.y) + 1;
+		case HORIZONTAL:
+			return ((end.x - start.x) / 2) + 1;
+		case NW_SE:
+			return (end.cartesianX() - start.cartesianX()) + 1;
+		case NE_SW:
+			return (end.cartesianY() - start.cartesianY()) + 1;
+		}
+		return 0;
+	}
+
+	bool contains(int x, int y) const {
+		switch (angle) {
+		case INVALID_LINE_ANGLE:
+			return false;
+		case VERTICAL:
+			return (x == start.x) && (y >= start.y) && (y <= end.y);
+		case HORIZONTAL:
+			return (y == start.y) && (x >= start.x) && (x <= end.x);
+		case NW_SE:
+			return (y >= start.y) && (y <= end.y) && start.intersectsLineNWSE(x, y);
+		case NE_SW:
+			return (y >= start.y) && (y <= end.y) && start.intersectsLineNESW(x, y);
+		}
+		return false;
+	}
+
+	bool tryAssign(const CMapPoint &newStart, const CMapPoint &newEnd) {
+		bool mustSwap = false;
+
+		if (newStart.x == newEnd.x) {
+			angle = VERTICAL;
+			mustSwap = (newStart.y > newEnd.y);
+		} else if (newStart.y == newEnd.y) {
+			angle = HORIZONTAL;
+			mustSwap = (newStart.x > newEnd.x);
+		} else if (newStart.intersectsLineNWSE(newEnd.x, newEnd.y)) {
+			angle = NW_SE;
+			mustSwap = (newStart.y > newEnd.y);
+		} else if (newStart.intersectsLineNESW(newEnd.x, newEnd.y)) {
+			angle = NE_SW;
+			mustSwap = (newStart.y > newEnd.y);
+		} else {
+			return false;
+		}
+
+		// if we got here, one of the angles must have been valid
+		if (mustSwap) {
+			end = newStart;
+			start = newEnd;
+		} else {
+			start = newStart;
+			end = newEnd;
+		}
+		return true;
+	}
+
+
+	void snapToPoints(const CMapPoint &anchor, const CMapPoint &other);
+};
+
+struct CDiamond {
+	CMapPoint top, bottom;
 };
 
 uint qHash(const CMapPoint &point);
 QDebug operator<<(QDebug dbg, const CMapPoint &point);
+QDebug operator<<(QDebug dbg, const CLine &line);
 
 #endif // CMAPPOINT_H
